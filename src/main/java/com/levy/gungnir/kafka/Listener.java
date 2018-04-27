bc6dc38036197d72962a0a795a1f4c5dadb882d4
linfei.ma <linfei.ma@chinapnr.com>
2018-04-27 15:01:31 +0800

    kafka 配置

--- src/main/java/com/levy/gungnir/kafka/Listener.java
+++ src/main/java/com/levy/gungnir/kafka/Listener.java
@@ -1,7 +1,9 @@
 package com.levy.gungnir.kafka;
 
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
 
 
 public class Listener {
@@ -10,11 +12,11 @@ public class Listener {
     /**
      *
      * @param record
-
+    */
     @KafkaListener(topics = {"test"})
     public void listen(ConsumerRecord<?, ?> record) {
         logger.info("kafka的key: " + record.key());
         logger.info("kafka的value: " + record.value().toString());
     }
-     */
+
 }
